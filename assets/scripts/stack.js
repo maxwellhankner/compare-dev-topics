f39ae3eff45f682b48727146292b044dddbaa95d
David Midlo <dmidlo@gmail.com>
2020-03-17 15:04:24 -0500

    commiting working changes to pull master

--- assets/scripts/stack.js
+++ assets/scripts/stack.js
@@ -14,14 +14,18 @@ var GetStackOverflow = function() {
   this.buildQueryString = function(tagArray, type) {
     var queryURL;
     // a switch is used here to achieve the effect "if/else && always"
+    // switch(true) is used here to allow each case to be processed as well as in this particular order.
     switch (true) {
-      // Always remove a trailing comma (,) or semicolon (;). No 'break' and 'on-top' for 'always' behavior.
+      case typeof tagArray == undefined:
+        return tagArray;
+      // Always remove a trailing comma (,) or semicolon (;). No 'break' is used here
+      // and positionally it is placed 'on-top' to achieve 'always' behavior.
       case tagArray[tagArray.length - 1] === "," ||
         tagArray[tagArray.length - 1] === ";":
         tagArray = tagArray.slice(0, -1);
       // if variable named 'array' is indeed an array of non-zero length
       // or, if it a string that includes a comma or semicolon:
-      //   -
+      //   - stringify the input.
       case (Array.isArray(tagArray) === true && tagArray.length !== 0) ||
         (typeof tagArray === "string" &&
           (tagArray.includes(",") || tagArray.includes(";"))):
@@ -33,11 +37,17 @@ var GetStackOverflow = function() {
       default:
     }
 
-    return queryURL;
+    //return queryURL;
+  };
+  this.buildQueryStringEmptyException = function(value) {
+    this.value = value;
+    this.message = "input cannot be empty";
+    this.toString = function() {
+      return this.value + this.message;
+    };
   };
 
   this.tags = function(array) {
-    console.log(queryURL);
     // $.ajax({
     //   url: queryURL,
     //   type: "GET",
@@ -49,5 +59,5 @@ var GetStackOverflow = function() {
 };
 var stackOverflow = new GetStackOverflow();
 
-console.log(stackOverflow.buildQueryString(["javascript", "reactjs"], "tags"));
+console.log(stackOverflow.buildQueryString([], "tags"));
 //stackOverflow.buildQueryString(["javascript", "reactjs"], "questions");
