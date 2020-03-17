be6b7e3d1dd77d711c270a2a920be3420bcadb1f
David Midlo <dmidlo@gmail.com>
2020-03-17 13:28:59 -0500

    commiting working changes

--- assets/scripts/stack.js
+++ assets/scripts/stack.js
@@ -7,25 +7,27 @@ var GetStackOverflow = function() {
       .split(",")
       .join(";");
   };
-  this.buildQueryString = function(array, type) {
-    //////////////////////////////////////////////////////////////////////////
-    //set queryURL based on input type.
-    //    - Accepts an array, single-item array,
+
+  //////////////////////////////////////////////////////////////////////////
+  //set queryURL based on reponse type.
+  //    - Accepts an array, single-item array, single sting, or stringfied array.
+  this.buildQueryString = function(tagArray, type) {
     var queryURL;
     // a switch is used here to achieve the effect "if/else && always"
     switch (true) {
       // Always remove a trailing comma (,) or semicolon (;). No 'break' and 'on-top' for 'always' behavior.
-      case array[array.length - 1] === "," || array[array.length - 1] === ";":
-        array = array.slice(0, -1);
+      case tagArray[tagArray.length - 1] === "," ||
+        tagArray[tagArray.length - 1] === ";":
+        tagArray = tagArray.slice(0, -1);
       // if variable named 'array' is indeed an array of non-zero length
       // or, if it a string that includes a comma or semicolon:
       //   -
-      case (Array.isArray(array) === true && array.length !== 0) ||
-        (typeof array === "string" &&
-          (array.includes(",") || array.includes(";"))):
+      case (Array.isArray(tagArray) === true && tagArray.length !== 0) ||
+        (typeof tagArray === "string" &&
+          (tagArray.includes(",") || tagArray.includes(";"))):
         var queryURL =
           "https://api.stackexchange.com/2.2/tags/%7B" +
-          this.buildStringList(array) +
+          this.buildStringList(tagArray) +
           "%7D/synonyms?order=desc&sort=creation&site=stackoverflow";
         break;
       default:
@@ -33,6 +35,7 @@ var GetStackOverflow = function() {
 
     return queryURL;
   };
+
   this.tags = function(array) {
     console.log(queryURL);
     // $.ajax({
@@ -46,5 +49,5 @@ var GetStackOverflow = function() {
 };
 var stackOverflow = new GetStackOverflow();
 
-console.log(stackOverflow.buildQueryString(["javascript","reactjs"], "tags"));
-stackOverflow.buildQueryString(["javascript", "reactjs"], "questions");
+console.log(stackOverflow.buildQueryString(["javascript", "reactjs"], "tags"));
+//stackOverflow.buildQueryString(["javascript", "reactjs"], "questions");
