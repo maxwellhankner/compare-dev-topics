b0ba770592bcd801d834eb253cdfc99c5206b18c
Maxwell Hankner <maxwellhankner@gmail.com>
2020-03-17 15:28:09 -0500

    got chart build functions working

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,4 @@
-// Select page elements
+// -------------------------------------------------- Select page elements
 // Containers
 var welcomeContainer = $('#welcome-container');
 var chartResultsContainer = $('#chart-results-container');
@@ -9,7 +9,7 @@ var searchField = $('#search-input-field');
 // Page title
 var pageTitle = $('#page-title');
 
-// -------------------------------------------------- Navigation and Views
+// -------------------------------------------------- Navigation and View Management
 // Hidden on page launch
 chartResultsContainer.attr('style', 'display: none;');
 
@@ -42,43 +42,78 @@ searchButton.click(function(event){
     // Build two arrays
 
     // Run buildChart function with static parameters
-    buildChart(['react'], [10000]);
+    buildGithubChart(['react'], [10000]);
+    buildStackOverflowChart(['react'], [8700]);
+
+    // buildGithubChart(['react', 'angular'], [9000, 7600]);
+    // buildStackOverflowChart(['react', 'angular'], [12000, 5700]);
 })
 
-// -------------------------------------------------- Chart Function
-
-let myChart = document.getElementById('my-chart').getContext('2d');
-function buildChart(topics, githubResults){
+// -------------------------------------------------- Chart Functions
+// Select github canvas
+let githubChart = document.getElementById('github-chart').getContext('2d');
+// Github Chart Function
+function buildGithubChart(topics, githubResults){
+    // Create empty array for datasets:
     var githubDataSet = [];
-    // var stackOverflowDataSet = [];
+    // Loop the topics and populate dataset array
     for(i = 0; i < topics.length; i++){
         var topicDataObject = {
             label: topics[i],
-            data: githubResults[i]
+            data: [githubResults[i]]
         }
-        console.log(topicDataObject);
         githubDataSet.push(topicDataObject);
     }
-    console.log(githubDataSet);
-    if(topics.length = 1){
-        let barChart = new Chart(myChart, {
-            type: 'bar',
-            data: {
-                labels: ['Repositories'],
-                datasets: [{
-                    label: topics,
-                    data: results
+    // Build the chart according to githubDataSet
+    let barChart = new Chart(githubChart, {
+        type: 'bar',
+        data: {
+            labels: ['Questions'],
+            datasets: githubDataSet
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
                 }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
             }
-        });
+        }
+    });
+    
+}
+
+// Select stack overflow canvas
+let stackOverflowChart = document.getElementById('stack-overflow-chart').getContext('2d');
+// Stack Overflow Chart Function
+function buildStackOverflowChart(topics, stackOverflowResults){
+    // Create empty array for datasets:
+    var stackOverflowDataSet = [];
+    // Loop the topics and populate dataset array
+    for(i = 0; i < topics.length; i++){
+        var topicDataObject = {
+            label: topics[i],
+            data: [stackOverflowResults[i]]
+        }
+        stackOverflowDataSet.push(topicDataObject);
     }
+    // Build the chard according to stackOverflowDataSet
+    let barChart = new Chart(stackOverflowChart, {
+        type: 'bar',
+        data: {
+            labels: ['Repositories'],
+            datasets: stackOverflowDataSet
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+    
 }
