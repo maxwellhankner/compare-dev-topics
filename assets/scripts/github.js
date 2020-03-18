2298315a212065b49ebdc1151098efb49a0ac1f5
James Berg <james.berg38@yahoo.com>
2020-03-18 13:17:07 -0500

    created getHubReposCount function. Currently does not use promises to get around ajax asyncronous behavior

--- assets/scripts/github.js
+++ assets/scripts/github.js
@@ -1,21 +1,40 @@
 // START document ready function
+$(document).ready(function(){
 
-    // START Github API function call
+    
+    console.log("ln 5 gitHubReposCount: "+ getGitHubReposCount("javascript"))
 
+
+    // START searchGitHubRepos function
+    function getGitHubReposCount(searchValue){
+        
+        var count = 0;
+        
         // create queryString variable for GitHub API call
+        var gitHubQueryString = "https://api.github.com/search/repositories?q=language:"+searchValue+"&sort=stars&order=desc";
+        
+        console.log(gitHubQueryString)
 
         // START gitHub API call
-
-            // START anonymous callback function on GET success
-
-                // create data object from response
-
-                // call renderGitHubDetails function
+        $.ajax({          
             
-            // END anonymous callback function on GET success
+            // use GET to return api data
+            type: "GET",
+            // queryString url
+            url: gitHubQueryString,
+            // data returned will be in json format
+            dataType: "json"  
+        }).then(function(response) {
 
+            console.log("ln 29 response.total_count: " + response.total_count);
+            count = response.total_count;
+
+        });
         // END gitHub API call
 
-    // END Github API function call
+        return count;
 
+    }
+    // END searchGitHubRepos function
+})
 // START document ready function
