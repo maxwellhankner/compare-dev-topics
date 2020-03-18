e6d083e8a247236103725e52588fa6254272e718
Maxwell Hankner <maxwellhankner@gmail.com>
2020-03-18 14:09:17 -0500

    async await testing

--- assets/scripts/github.js
+++ assets/scripts/github.js
@@ -1,40 +1,39 @@
 // START document ready function
-$(document).ready(function(){
 
     
-    console.log("ln 5 gitHubReposCount: "+ getGitHubReposCount("javascript"))
+// console.log("ln 5 gitHubReposCount: "+ getGitHubReposCount("javascript"))
 
 
-    // START searchGitHubRepos function
-    function getGitHubReposCount(searchValue){
+// START searchGitHubRepos function
+async function getGitHubReposCount(searchValue){
+    
+    var count = 0;
+    
+    // create queryString variable for GitHub API call
+    var gitHubQueryString = "https://api.github.com/search/repositories?q=language:"+searchValue+"&sort=stars&order=desc";
+    
+    console.log(gitHubQueryString)
+
+    // START gitHub API call
+    await $.ajax({          
         
-        var count = 0;
-        
-        // create queryString variable for GitHub API call
-        var gitHubQueryString = "https://api.github.com/search/repositories?q=language:"+searchValue+"&sort=stars&order=desc";
-        
-        console.log(gitHubQueryString)
+        // use GET to return api data
+        type: "GET",
+        // queryString url
+        url: gitHubQueryString,
+        // data returned will be in json format
+        dataType: "json"  
+    }).then(function(response) {
 
-        // START gitHub API call
-        $.ajax({          
-            
-            // use GET to return api data
-            type: "GET",
-            // queryString url
-            url: gitHubQueryString,
-            // data returned will be in json format
-            dataType: "json"  
-        }).then(function(response) {
+        console.log("ln 29 response.total_count: " + response.total_count);
+        count = response.total_count;
 
-            console.log("ln 29 response.total_count: " + response.total_count);
-            count = response.total_count;
+    });
+    // END gitHub API call
 
-        });
-        // END gitHub API call
+    return count;
 
-        return count;
+}
+// END searchGitHubRepos function
 
-    }
-    // END searchGitHubRepos function
-})
 // START document ready function
